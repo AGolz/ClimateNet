090282e884bbc129cf6e1910adc2ed4b2bad18f5
Elena Maksimova <emaksimo@c2r9s2.42yerevan.am>
2023-03-22 18:47:32 +0400

    climate/button

---
+++ button.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+BUTTON_PIN = 17
+
+LED_PIN = 18
+
+GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(LED_PIN, GPIO.OUT)
+
+while True:
+    if GPIO.input(BUTTON_PIN) == GPIO.LOW:
+        GPIO.output(LED_PIN, GPIO.HIGH)
+        print("Button pressed!")
+    else:
+        GPIO.output(LED_PIN, GPIO.LOW)
+    time.sleep(0.1)
