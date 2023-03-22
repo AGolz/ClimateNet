77f0a33d9c93b547f8b5a66f789273180e79e79e
Alyona Golz <emax@coex.tech>
2023-03-22 18:50:40 +0400

    Update ledrpi.py

--- ledrpi.py
+++ ledrpi.py
@@ -14,15 +14,9 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-
 LED_PIN = 18
-
 GPIO.setup(LED_PIN, GPIO.OUT)
-
 GPIO.output(LED_PIN, GPIO.HIGH)
-
 time.sleep(5)
-
 GPIO.output(LED_PIN, GPIO.LOW)
-
 GPIO.cleanup()
